477ae108a5e946a1a79b8d7ae0af9fa72ba1b621
isayahc <isayahculbertson@gmail.com>
2023-05-31 02:40:32 -0400

    Enhance Python function analysis script: add docstrings, type annotations, and improve error handling

--- function_names.py
+++ function_names.py
@@ -1,13 +1,26 @@
+"""
+A Python script to analyze another Python file and extract details about its functions and methods.
+
+Usage: python analyze_python_file.py <file-to-analyze.py>
+"""
+
 import ast
 import argparse
 import os
+from typing import Optional, Dict, Any
 
 
-def get_func_details(node, class_name=None):
+def get_func_details(node: ast.AST, class_name: Optional[str] = None) -> Dict[str, Any]:
+    """
+    Recursively traverse an AST node to extract function/method details.
+
+    :param node: The AST node to analyze
+    :param class_name: The name of the class if the node is a method
+    :return: A dictionary with function/method details
+    """
     func_details = {}
 
     if isinstance(node, ast.FunctionDef):
-        # Extract function/method details
         func_name = f"{class_name}.{node.name}" if class_name else node.name
         func_details[func_name] = {
             'args': {arg.arg: ast.unparse(arg.annotation) if arg.annotation else None for arg in node.args.args},
@@ -15,11 +28,9 @@ def get_func_details(node, class_name=None):
             'docstring': ast.get_docstring(node)
         }
     elif isinstance(node, ast.ClassDef):
-        # Extract class method details
         for sub_node in node.body:
             func_details.update(get_func_details(sub_node, class_name=node.name))
 
-    # Check for any nested functions or classes
     if isinstance(node, ast.Module):
         for sub_node in node.body:
             func_details.update(get_func_details(sub_node))
@@ -27,18 +38,22 @@ def get_func_details(node, class_name=None):
     return func_details
 
 
-def analyze_python_file(filepath):
-    # Check if the file exists
+def analyze_python_file(filepath: str) -> Dict[str, Any]:
+    """
+    Analyze a Python file and extract details about its functions and methods.
+
+    :param filepath: The path to the Python file
+    :return: A dictionary with function/method details
+    :raises ValueError: If the file does not exist, is not a Python file, or contains syntax errors
+    """
     if not os.path.isfile(filepath):
         raise ValueError(f"{filepath} does not exist")
 
-    # Check if the file is a Python file
     if not filepath.endswith('.py'):
         raise ValueError(f"{filepath} is not a Python file")
 
     with open(filepath, 'r') as file:
         try:
-            # Parse the file to an Abstract Syntax Tree (AST)
             module = ast.parse(file.read())
         except SyntaxError as e:
             raise ValueError(f"{filepath} contains syntax errors: {str(e)}")
@@ -49,7 +64,6 @@ def analyze_python_file(filepath):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get function details from a Python file.')
     parser.add_argument('file', type=str, help='The Python file to analyze.')
-
     args = parser.parse_args()
 
     try:
